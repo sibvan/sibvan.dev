c092ad803723b744b86c7e6b69de8de2e047f93f
sibvan <sibvan.dev@gmail.com>
2025-06-27 21:04:54 +0400

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import { setHeroMargin } from "./hero.js";
 import { changeText } from "./projects.js";
 const linkEl = document.querySelector("[data-js-sibvan]");
-document.querySelector(".burqa").style.opacity = 0;
 
 window.addEventListener("resize", setHeroMargin);
-window.addEventListener("load", setHeroMargin);
+window.addEventListener("load", () => {
+  document.querySelector(".burqa").style.opacity = 0;
+  setHeroMargin();
+});
 linkEl.addEventListener("click", (clickEvent) => {
   clickEvent.preventDefault();
   changeText(linkEl.textContent.slice(0, -2));
